6ff6ea32d562d4a60d3a13c427da3b3e18069ca6
kim-yechen <wsxft945@naver.com>
2026-04-04 19:09:10 +0900

    Update sentry.py

--- sentry.py
+++ sentry.py
@@ -364,7 +364,7 @@ class QuantumControlCenter:
     # --------------------------------------------------------------------------
     def process_floor_1(self):
         is_v8_dominant = self.v8_p >= 60.0
-        portfolio = ['FCX', 'SCCO','TGB', 'SIVR', 'ISSC', 'LUNR', 'TDW', 'SLB', 'SIDU']
+        portfolio = ['FCX', 'TGB', 'SIVR', 'ISSC', 'TDW', 'SLB', 'SIDU']
         results = []
         
         try:
